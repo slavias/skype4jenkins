93cde78b25982386dcbcac4a31fd02e295bfba66
Viacheslav Tkach <viacheslav_tkach@epam.com>
2016-12-01 15:21:58 +0200

    add remote control function

--- src/main/java/com/skype/jenkins/RunNotification.java
+++ src/main/java/com/skype/jenkins/RunNotification.java
@@ -18,6 +18,8 @@ import com.skype.jenkins.logger.Logger;
 public class RunNotification {
 
     private static List<ConfigDTO> configData;
+    private static ScheduledExecutorService threadService;
+
     private static List<ConfigDTO> initConfData() {
         String confPath = Optional.ofNullable(System.getProperty("config.file"))
                 .orElseThrow(() -> new RuntimeException("Specify config.file property"));
@@ -30,14 +32,46 @@ public class RunNotification {
         return Optional.ofNullable(configData).orElseGet(RunNotification::initConfData);
     }
 
+    public static ScheduledExecutorService getThreadService() {
+        return Optional.ofNullable(threadService)
+                .map(srv -> srv.isShutdown() ? null : srv)
+                .orElseGet(() -> {
+                            threadService = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
+                            return threadService;
+                        });
+    }
+
     //TODO implement message on time
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args){
         SkypeHelper.getSkype();
-        ScheduledExecutorService service = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
-        getConfiguration().stream().forEach(
-                allConf -> allConf.getJobs().forEach(
-                        jobConf -> service.scheduleWithFixedDelay(new JobThread(allConf.getJenkinsUrl(), jobConf), 1,
-                                jobConf.getInfo().getTimeout(), TimeUnit.SECONDS)));
+        getThreadService().shutdown();
+        start();
+    }
+
+    public static String start() {
+        if (threadService.isShutdown()){
+            getConfiguration().stream().forEach(
+                    allConf -> allConf.getJobs().forEach(
+                            jobConf -> getThreadService().scheduleWithFixedDelay(new JobThread(allConf.getJenkinsUrl(), jobConf), 1,
+                                    jobConf.getInfo().getTimeout(), TimeUnit.SECONDS)));
+            return "Start listener";
+        } else {
+            return "Listener already started. Can't start";
+        }
+    }
+
+    public static String stop() {
+        if (threadService.isShutdown()){
+            return "Listener not running. Can't stop";
+        } else{
+            configData = null;
+            threadService.shutdown();
+            return "Stop Listener";
+        }
+    }
+
+    public static String status() {
+        return threadService.isShutdown() ?"Listener stopped" : "Listener Started";
     }
 
     public static ConfigDTO parseConfigFile(final String configName) {
--- src/main/java/com/skype/jenkins/SkypeHelper.java
+++ src/main/java/com/skype/jenkins/SkypeHelper.java
@@ -3,15 +3,20 @@ package com.skype.jenkins;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import com.samczsun.skype4j.Skype;
 import com.samczsun.skype4j.SkypeBuilder;
 import com.samczsun.skype4j.chat.GroupChat;
+import com.samczsun.skype4j.events.EventHandler;
+import com.samczsun.skype4j.events.Listener;
+import com.samczsun.skype4j.events.chat.message.MessageReceivedEvent;
 import com.samczsun.skype4j.exceptions.ChatNotFoundException;
 import com.samczsun.skype4j.exceptions.ConnectionException;
 import com.samczsun.skype4j.exceptions.InvalidCredentialsException;
 import com.samczsun.skype4j.exceptions.NotParticipatingException;
 import com.samczsun.skype4j.internal.StreamUtils;
+import com.skype.jenkins.dto.ConfigDTO;
 import com.skype.jenkins.logger.Logger;
 
 import static com.skype.jenkins.logger.Logger.stackTrace;
@@ -31,6 +36,10 @@ public class SkypeHelper {
                 skype = new SkypeBuilder(data[0], data[1]).withAllResources()
                         .withLogger(java.util.logging.Logger.getLogger("skypeLogger")).build();
                 skype.login();
+                if (Boolean.parseBoolean(System.getProperty("remote.control"))) {
+                    skype.getEventDispatcher().registerListener(new MessageListener());
+                    skype.subscribe();
+                }
             } catch (InvalidCredentialsException | ConnectionException | NotParticipatingException | IOException e) {
                Logger.out.error("Caught exception getSkype()  " + stackTrace(e));
             }
@@ -64,5 +73,57 @@ public class SkypeHelper {
             }
         } while ((illegalStateException != null) && (i < resendTry));
     }
+
+    private static class MessageListener implements Listener {
+
+        @EventHandler
+        public void onMessage(final MessageReceivedEvent e) throws ConnectionException  {
+            if (e.getMessage().getContent().asPlaintext().startsWith("//")) {
+                Logger.out.info("COMMAND FROM "+e.getMessage().getSender().getDisplayName()+": " + e.getChat().getIdentity() + "(" + ((GroupChat) e.getChat()).getTopic() + ")\n" + e.getMessage().getContent());
+                sendSkype(execCommands(e.getMessage().getContent().asPlaintext().substring(2)), e.getChat().getIdentity());
+            }
+
+        }
+
+        private String execCommands(final String command) {
+            StringBuilder output = new StringBuilder();
+            switch (command) {
+            case "help":
+                output.append("//jobs - list of job to listeners").append("\n");
+                output.append("//jenkins - jenkins host(s) to listen").append("\n");
+                output.append("//stop").append("\n");
+                output.append("//start").append("\n");
+                output.append("//reload(restart) - reload config file").append("\n");
+                output.append("//status - reload config file").append("\n");
+                break;
+            case "jobs":
+                output.append(RunNotification.getConfiguration().stream().flatMap(conf -> conf.getJobs().stream()).map(jobConf -> jobConf.getInfo().getJobName()).collect(Collectors.joining("\n")));
+                break;
+            case "jenkins":
+                output.append(RunNotification.getConfiguration().stream().map(ConfigDTO::getJenkinsUrl).collect(Collectors.joining("\n")));
+                break;
+            case "reload":
+            case "restart":
+                output.append(RunNotification.stop());
+                output.append("\n");
+                output.append(RunNotification.start());
+                break;
+            case "stop":
+                output.append(RunNotification.stop());
+                break;
+            case "start":
+                output.append(RunNotification.start());
+                break;
+            case "status":
+                output.append(RunNotification.status());
+                break;
+            default: output.append("No such command. Use:\n").append(execCommands("help"));
+            }
+
+            return "".equals(output.toString())?"No results found":output.toString();
+        }
+    }
+
+
 
 }
